3607b621c2f5b06bb0423593ed21491606366793
Rafael Takahashi <rafael.takahashi@germinare.org.br>
2024-09-25 16:23:59 -0300

    Tela login administrador finalizada

--- LandingPage.js
+++ LandingPage.js
@@ -1,12 +1,17 @@
 const senhaInput = document.getElementById('senha');
-const mostrarSenha = document.querySelector('.mostrar-senha');
+const mostrarSenha = document.querySelector('.mostrar-senha img');
+
+const olhoAberto = 'Icone_olhoA.png';
+const olhoFechado = 'Icone_olhoB.png';
 
 mostrarSenha.addEventListener('click', () => {
     if (senhaInput.type === 'password') {
         senhaInput.type = 'text';
-        mostrarSenha.textContent = 'Ocultar';
+        mostrarSenha.src = olhoFechado;
+        mostrarSenha.alt = 'Ocultar senha';
     } else {
         senhaInput.type = 'password';
-        mostrarSenha.textContent = 'Mostrar';
+        mostrarSenha.src = olhoAberto;
+        mostrarSenha.alt = 'Mostrar senha';
     }
 });
