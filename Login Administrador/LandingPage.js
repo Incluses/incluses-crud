b44731dfdfa1867e9c2c505107064c9f3445643c
Diogobq7 <diogo.queiroz@germinare.org.br>
2024-10-24 11:04:53 -0300

    Adicionando as regex e conectando as páginas

--- Login Administrador/LandingPage.js
+++ Login Administrador/LandingPage.js
@@ -1,8 +1,16 @@
+const form = document.getElementById('formLogin');
 const senhaInput = document.getElementById('senha');
-const mostrarSenha = document.querySelector('.mostrar-senha img');
+const erroSenha = document.getElementById('erroSenha');
+const erroAdm = document.getElementById('erroAdm');
+const usuarioInput = document.getElementById('usuario');
 
+const mostrarSenha = document.querySelector('.mostrar-senha img');
 const olhoAberto = 'Icone_olhoA.png';
 const olhoFechado = 'Icone_olhoB.png';
+
+// Regex para validação
+const patternSenha = /^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])(?=.*[áàâãéèêíïóôõöú])?(?=.*[\!\@\#\$\%\^\&\\(\)\_\-\+\=\[\]\{\}\|\;\:\'\"\,\.\<\>\/\?]).{8,}$/;
+const patternAdm = /^adm((?=.[A-Z]{1,})?|(?=.[a-z]{1,}))(?=.\d{1,})?(?=.[!@#$%^&*\(\)_\-+=[]\{\}\|;:'",\.<>\/\?])?.{3,}/;
 
 mostrarSenha.addEventListener('click', () => {
     if (senhaInput.type === 'password') {
@@ -15,3 +23,30 @@ mostrarSenha.addEventListener('click', () => {
         mostrarSenha.alt = 'Mostrar senha';
     }
 });
+
+// Validação do login
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    erroAdm.textContent = '';
+    erroSenha.textContent = '';
+
+    let isValid = true;
+
+    // Validação do ADM
+    if (!patternAdm.test(usuarioInput.value)) {
+        erroAdm.textContent = 'ADM inválido! O nome não atende aos padrões requisitados.';
+        isValid = false;
+    }
+
+    // Validação da senha
+    if (!patternSenha.test(senhaInput.value)) {
+        erroSenha.textContent = 'Senha inválida! A senha não atende aos padrões requisitados.';
+        isValid = false;
+    }
+    
+    if (isValid) {
+        alert('Login realizado com sucesso!');
+        window.location.href = '../CrudAdm/adm.html';
+    }
+})
